48c1d3768e78dcd022ef6aa661777df72a028adf
astechutube <astech.utube@gmail.com>
2022-08-19 19:38:47 +0530

    java file

--- index.java
+++ index.java
@@ -1,10 +1,10 @@
 import java.util.*;
 
-public class Main {
+public class Person {
   int x = 5;
 
   public static void main(String[] args) {
-    Main myObj = new Main();
+    Person myObj = new Person();
     System.out.println(myObj.x);
   }
 }
