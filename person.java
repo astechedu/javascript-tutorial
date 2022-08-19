41702362dda292a1f8db25f6e55f1c7dd39ffd29
astechutube <astech.utube@gmail.com>
2022-08-19 22:15:52 +0530

    java file

---
+++ person.java
@@ -0,0 +1,10 @@
+  import java.io.*;
+
+  class Person {
+  int x = 5;
+
+  public static void main(String[] args) {
+    Person myObj = new Person();
+    System.out.println("Number: " + myObj.x);
+  }
+}
