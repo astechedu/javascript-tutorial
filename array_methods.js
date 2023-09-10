ab38cb5ce8665055eb426fcb777d20636872ecff
= <=>
2023-09-10 13:09:51 -0400

    Js Tutorial

---
+++ array_methods.js
@@ -0,0 +1,6 @@
+//Array Methods
+
+//length,toString(),pop(),push(),shift(),unshift(),join(),delete(),concat(),flat(),splice(),slice(),reverse(),sort(),fill(),lastIndexOf(),indexOf(),of(),
+//findIndex(),find(),includes(),reduceRight(),isArray(),filter(),map(),forEach(),Array.every(),entries(),keys(),values(),Array.from(),constructor(),
+//copyWithin(),Array.toLocaleString(),flatMap(),
+
--- string_methods.js
+++ string_methods.js
@@ -1,1 +1,5 @@
 //In-built String Methods
+
+//String Methods to be done
+// length,slice(),substring(),substr(),replace(), replaceAll(),toUpperCase(),toLowerCase(),concat(),trim(),trimStart(),trimEnd(),padStart(),
+//padEnd(),charAt(),charCodeAt(),split(),indexOf(),lastIndexOf(),search(),match(),matchAll(),includes(),startsWith(),endsWith()
