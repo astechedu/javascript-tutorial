7dcf66fbd589c6019ea4af9a9485c38b0a51db3f
= <=>
2023-09-14 11:47:09 -0400

    Js Tutorial

---
+++ js_tutorial.js
@@ -0,0 +1,55 @@
+//JavaScript Data Types
+//String,Number,Bigint,Boolean,Undefied,Null,Symbol,Object
+
+//Undefined
+let id;
+let id2=undefined
+//console.log(id)
+//console.log(id2)
+//console.log(typeof(id))
+
+//Null 
+let nl = null
+//console.log(nl)
+//console.log(typeof(nl))
+
+// 1. Numbers:
+//let length = 18;
+//let weight = 5.5;
+//console.log("Numberr: " + length)
+//console.log("Weight: " + weight)
+//console.log(typeof(weight))
+
+//2. Strings:
+//let color = "Red";
+//let lastName = "Singh";
+//console.log(color)
+//console.log(lastName)
+//console.log(typeof(color))
+
+//3. Booleans
+//let x = true;
+//let y = false;
+//console.log("x: " + x)
+//console.log("y: " + y)
+//console.log(typeof(x))
+
+
+//4. Object:
+//const person = {firstName:"ajay", lastName:"sisaudiya"};
+//console.log(person)
+//console.log(typeof(person))
+//console.log(person["firstName"])
+//console.log(person.firstName)  
+
+
+//5. Array object:
+const cars = ["Volvo", "Maruti", "BMW"];
+//console.log(cars)
+//console.log(cars[0])
+//console.log(typeof(cars))
+
+//6. Date object:
+const date = new Date("2023-03-25"); 
+//console.log(date)
+//console.log(typeof(date))
