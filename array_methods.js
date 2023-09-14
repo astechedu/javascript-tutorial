e9f72859fe80121354a05a969ce7a63c5268a04f
= <=>
2023-09-14 08:20:39 -0400

    Js Tutorial

--- array_methods.js
+++ array_methods.js
@@ -4,3 +4,8 @@
 //findIndex(),find(),includes(),reduceRight(),isArray(),filter(),map(),forEach(),Array.every(),entries(),keys(),values(),Array.from(),constructor(),
 //copyWithin(),Array.toLocaleString(),flatMap(),
 
+
+let x
+x=true
+console.log(x)
+console.log(typeof(x))
--- index.js
+++
@@ -1,1 +0,0 @@
-console.log("JavaScript Tutorial For Beginners");
